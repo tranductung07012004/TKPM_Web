c2d227dff1f38ed08e0d2512d61b65d5102870af
Trần Tiến Lợi <trantienloi2404@gmail.com>
2025-02-27 10:58:39 +0700

    add PDP page to app

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import PDP from './pages/PDP';
 function App() {
-    return (
-      <>
-        
-      </>
-    )
-  }
-  
-  export default App
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<PDP />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
